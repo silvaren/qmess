d7edf24c6670914fcf29c9b2a4bdc91ef54640fe
Renato Oliveira <oliveirasilvaren@gmail.com>
2017-03-12 18:45:49 -0300

    Initial structure

---
+++ src/main/scala-2.12/Queue.scala
@@ -0,0 +1,8 @@
+
+class Queue {
+
+  // SendMessage: Send messages to a specified queue.
+  // ReceiveMessage: Return one or more messages from a specified queue.
+  // DeleteMessage: Remove a previously received message from a specified queue.
+
+}

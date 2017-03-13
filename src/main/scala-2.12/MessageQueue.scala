53c262a26294bcf0d26e609ea1aeba85dc07815a
Renato Oliveira <oliveirasilvaren@gmail.com>
2017-03-13 00:27:11 -0300

    Add delete message implementaiton

--- src/main/scala-2.12/MessageQueue.scala
+++ src/main/scala-2.12/MessageQueue.scala
@@ -6,17 +6,18 @@ class MessageQueue {
   private var receivedMessages = 0
   private var queue = Seq[Message]()
 
-  // SendMessage: Send messages to a specified queue.
+  // SendMessage: Send messages to the queue
   def sendMessage(content: String): Unit = {
     receivedMessages += 1
     queue = Message(receivedMessages, content) +: queue
   }
 
-  // ReceiveMessage: Return one or more messages from a specified queue.
+  // ReceiveMessage: Return one or more messages from the queue.
   def receiveMessage(): Option[Message] =
     queue.headOption
 
-  // DeleteMessage: Remove a previously received message from a specified queue.
-  def deleteMessage(id: Int): Unit = ???
+  // DeleteMessage: Remove a previously received message from the queue.
+  def deleteMessage(id: Int): Unit =
+    queue = queue.filter(msg => msg.id != id)
 
 }
